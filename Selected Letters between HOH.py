4c23e4387ab70f6b090bf3119656706cf8cb5248
DeutschMark <mirque84@gmail.com>
2014-02-18 11:30:41 +0100

    New Tab with selected glyphs between "HOH"

---
+++ Selected Letters between HOH.py
@@ -0,0 +1,21 @@
+#MenuTitle: Tab with selected letters between HOH
+# -*- coding: utf-8 -*-
+"""Opens a new tab with the selected letters and put them between a string of HOH."""
+
+import GlyphsApp
+
+Doc = Glyphs.currentDocument
+Font = Glyphs.font
+selectedLayers = Font.selectedLayers
+
+keyChars = "/H/O/H"
+
+namesOfSelectedGlyphs = ["/" + l.parent.name for l in selectedLayers]
+editString = ""
+
+editString += keyChars
+for GlyphName in namesOfSelectedGlyphs:
+	editString += ( GlyphName + keyChars )
+
+print output, editString
+Doc.windowController().addTabWithString_( editString )
