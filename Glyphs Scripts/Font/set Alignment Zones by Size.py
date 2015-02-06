b49f7bcf7f520e236fa6799bf99a777aeafaceb2
DeutschMark <mirque84@gmail.com>
2015-02-06 11:02:16 +0100

    added GUI

--- Glyphs Scripts/Font/set Alignment Zones by Size.py
+++ Glyphs Scripts/Font/set Alignment Zones by Size.py
@@ -6,41 +6,65 @@ let me know if you have ideas for improving;
 Mark Froemberg aka DeutschMark @ GitHub
 https://github.com/DeutschMark/Glyphsapp-Scripts
 '''
+import vanilla
 
-font = Glyphs.currentDocument.font
-master = font.masters[0]
+Doc = Glyphs.currentDocument
+Font = Glyphs.font
+Selection = Font.selectedLayers
+#help(Font)
 
-### this is your desired zone size:
-size = 13
+#print Font.fontMasters
+#print Font.masters
 
-### -------------------------------
-### the untouchable code:
-### get the dimensions of the font
-posA = master.ascender
-posC = master.capHeight
-posX = master.xHeight
-posB = 0
-posD = master.descender
+class Window( object ):
+    def __init__( self ):
+        InputPosX = 90
+        InputWidth = 50
+        textHeight = 40
+        self.w = vanilla.FloatingWindow( (250, 45), "set Alignment Zones" )
+        
+        self.w.suffixText = vanilla.TextBox((10, 10, -10, textHeight), "AZ Size:")
+        self.w.suffixValue = vanilla.EditText((InputPosX, 10, InputWidth, 20), "15", sizeStyle='small')
 
-dimensions = [
-    (posA, size), 
-    (posC, size), 
-    (posX, size), 
-    (posB, -size), 
-    (posD, -size)
-]
+        self.w.make_button = vanilla.Button((-80, 12, -15, 17), "Create", sizeStyle='small', callback=self.addSuffix)
+    
+        self.w.open()
 
-newZones = []
-for d in dimensions:
-    pos, size = d
-    a = GSAlignmentZone.alloc().init()
-    a.setSize_(size)
-    a.setPosition_(pos)
-    newZones.append(a)
+		
+    def buttonCheck( self, sender ):
+        size = sender.get()
+        
 
-font.disableUpdateInterface()
+    def addSuffix(self, sender):
+    		size = int(self.w.suffixValue.get())
+    		print size
+    		
+		### the untouchable code:
+		### get the dimensions of the font
+		master = Font.masters[0]
+		posA = master.ascender
+		posC = master.capHeight
+		posX = master.xHeight
+		posB = 0
+		posD = master.descender
 
-master.setAlignmentZones_(newZones)
-#print master.alignmentZones
+		dimensions = [ (posA, size), (posC, size), (posX, size), (posB, -size), (posD, -size) ]
 
-font.enableUpdateInterface()
+		print dimensions
+		newZones = []
+		for d in dimensions:
+			pos, size = d
+			a = GSAlignmentZone.alloc().init()
+			a.setSize_(size)
+			a.setPosition_(pos)
+			newZones.append(a)	
+
+		#font.disableUpdateInterface()
+
+		master.setAlignmentZones_(newZones)
+		#print master.alignmentZones
+
+		#font.enableUpdateInterface()
+		self.w.close()
+		
+Window()
