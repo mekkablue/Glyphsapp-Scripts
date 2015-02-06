fd8e052980464a0ea6239f377b6e1f1a8beb56a3
DeutschMark <mirque84@gmail.com>
2015-02-06 15:12:53 +0100

    no message

---
+++ Glyphs Scripts/Glyphs/Note for Selection.py
@@ -0,0 +1,29 @@
+#MenuTitle: Note for Selection
+
+# ###################################################################
+#
+# adds a note to all selected characters
+# 
+# --> let me know if you have ideas for improving
+# --> Mark Froemberg aka DeutschMark @ GitHub <--
+#
+# ToDo: -
+#
+# ###################################################################
+
+Glyphs.clearLog()
+from robofab.interface.all.dialogs import AskString
+Note = AskString("What is your Note for the Selected Glyphs?")
+
+Doc = Glyphs.currentDocument
+Font = Glyphs.font
+selectedGlyphs = [ x.parent for x in Font.selectedLayers ]
+
+def process( thisGlyph ):
+	thisGlyph.setNote_(Note)
+	
+for thisGlyph in selectedGlyphs:
+	process( thisGlyph )
+	print "%s -->" % thisGlyph.name, Note
+
+Glyphs.showMacroWindow()
