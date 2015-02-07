1a682f54351ee79af807ddff2d6b62ee98b463c0
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2015-02-07 20:06:17 +0100

    Bugfixes, GUI improvements

--- Glyphs Scripts/Font/set Alignment Zones by Size.py
+++ Glyphs Scripts/Font/set Alignment Zones by Size.py
@@ -18,53 +18,50 @@ Doc = Glyphs.currentDocument
 Font = Glyphs.font
 Selection = Font.selectedLayers
 
-#print Font.fontMasters
-for master in Font.masters:
-	print master
-print
-
-font.disableUpdateInterface()
 class Window( object ):
-    def __init__( self ):
-        InputPosX = 90
-        InputWidth = 50
-        textHeight = 40
-        self.w = vanilla.FloatingWindow( (250, 45), "set Alignment Zones" )
-        
-        self.w.zonesText = vanilla.TextBox((10, 10, -10, textHeight), "AZ Size:")
-        self.w.zonesValue = vanilla.EditText((InputPosX, 10, InputWidth, 20), "15", sizeStyle='small')
-        self.w.make_button = vanilla.Button((-80, 12, -15, 17), "Create", sizeStyle='small', callback=self.makeZones)
-    
-        self.w.open()
-
-    def makeZones(self, sender):
-    	size = int(self.w.zonesValue.get())
-	
-    	### the untouchable code:
-    	### get the dimensions of the font
-    	for master in Font.masters:
-    		print master
-    		posA = master.ascender
-    		posC = master.capHeight
-    		posX = master.xHeight
-    		posB = 0
-    		posD = master.descender
-
-    		dimensions = [ (posA, size), (posC, size), (posX, size), (posB, -size), (posD, -size) ]
-
-    		print dimensions
-    		newZones = []
-    		for d in dimensions:
-    			pos, size = d
-    			a = GSAlignmentZone.alloc().init()
-    			a.setSize_(size)
-    			a.setPosition_(pos)
-    			newZones.append(a)	
-
-    		master.setAlignmentZones_(newZones)
-    		#print master.alignmentZones
-
-        font.enableUpdateInterface()
-	self.w.close()
+	def __init__( self ):
+		InputPosX = 90
+		InputWidth = 50
+		textHeight = 40
+		self.w = vanilla.FloatingWindow( (250, 45), "Set Alignment Zones" )
 		
+		self.w.zonesText = vanilla.TextBox((10, 10, -10, textHeight), "AZ Size:")
+		self.w.zonesValue = vanilla.EditText((InputPosX, 10, InputWidth, 20), "15", sizeStyle='small')
+		self.w.make_button = vanilla.Button((-80, 12, -15, 17), "Create", sizeStyle='small', callback=self.makeZones)
+
+		self.w.setDefaultButton( self.w.make_button )
+		self.w.open()
+		self.w.makeKey()
+
+	def makeZones(self, sender):
+		size = int(self.w.zonesValue.get())
+		self.w.close()
+
+		### the untouchable code:
+		### get the dimensions of the font
+		for master in Font.masters:
+			print master
+			posA = master.ascender
+			posC = master.capHeight
+			posX = master.xHeight
+			posB = 0
+			posD = master.descender
+
+			dimensions = [ (posA, size), (posC, size), (posX, size), (posB, -size), (posD, -size) ]
+
+			print dimensions
+			newZones = []
+			for d in dimensions:
+				pos, size = d
+				a = GSAlignmentZone.alloc().init()
+				a.setSize_(size)
+				a.setPosition_(pos)
+				newZones.append(a)	
+
+			master.setAlignmentZones_(newZones)
+			#print master.alignmentZones
+
+
+Font.disableUpdateInterface()		
 Window()
+Font.enableUpdateInterface()
