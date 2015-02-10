5bbd1f2194c92aa6829029763245233486287238
DeutschMark <mirque84@gmail.com>
2015-02-10 18:19:43 +0100

    new Script

---
+++ Sketching/fontmetrics-Glyph as lines (RoboFab).py
@@ -0,0 +1,49 @@
+#MenuTitle: Fontmetrics-Glyph
+
+# ###################################################################
+#
+# creates a new Glyphs with some lines according to the Font Metrics
+# (!) take care to have the GSPen.py & objectsGS.py on first level of
+# the glyphs scripts folder for the RoboFab to work
+# 
+# --> let me know if you have ideas for improving
+# --> Mark Froemberg aka DeutschMark @ GitHub <--
+#
+# ToDo: - deal with italic angle
+#
+# ###################################################################
+
+from robofab.world import CurrentFont
+import math
+f = CurrentFont()
+
+baseline = 0
+descender = f.info.descender
+xHeight = f.info.xHeight
+capHeight = f.info.capHeight
+ascender = f.info.ascender
+angle = f.info.italicAngle
+
+metrics = [baseline, descender, xHeight, capHeight, ascender]
+
+g = f.newGlyph("fontmetrics", True)
+
+p = g.getPen()
+print dir(p)
+w = 600
+
+u = 1
+gap = 10
+
+# rotation point is half of x-height
+offset = math.tan(math.radians(angle)) * xHeight/2
+    
+for m in metrics:
+    # offset for italic angle
+    shift = math.tan(math.radians(angle)) * m - offset
+	
+    p.moveTo((shift+gap, m))
+    p.lineTo((shift+gap, m+u))
+    p.lineTo((w+shift, m+u))
+    p.lineTo((w+shift, m))
+    p.closePath()
